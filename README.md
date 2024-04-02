247793648d62c4d8728b8594e72689574fb71da8
buiduchieu <duchieubui25012002@gmail.com>
2024-04-03 00:49:04 +0700

    add readme file and add scan.py file

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>command to run file</h1>
+
+example: python scan.py -a sqlinjection -u <url you want to detect>
---
+++ scan.py
@@ -0,0 +1,51 @@
+import sys
+import sqli_scanner as sqliscanner
+# def menus():
+#     print('''
+#     [+] 1. 
+#     [+] 2. Access Control
+#     [+] 3. SQL Injection
+#     [+] 4. 
+#     [+] 5. URL Brute force detection
+#     ''')
+
+# menus()
+print('''\033[92m
+    ______            _     _        _____  _   _  
+    |  _  \          | |   | |      / __  \| | | | 
+    | | | |___  _   _| |__ | | ___  `' / /'| |_| | 
+    | | | / _ \| | | | '_ \| |/ _ \   / /  |  _  | 
+    | |/ / (_) | |_| | |_) | |  __/ ./ /___| | | | 
+    |___/ \___/ \__,_|_.__/|_|\___| \_____/\_| |_/
+	                    recoded by BUIDUCHIEU
+''')
+
+attackNameString = [
+    {"-r":""}
+]
+
+helpCommand = """
+    Welcome to my tool and now how we run this tool. To run it please using command
+    and provide tag information.
+    -h: --help
+    -u: url string
+    -a: attack name, example: -a sqlinjection, -a accesscontrol, ... 
+"""
+
+getUrl = sys.argv[sys.argv.index('-u')+1]
+getAttackName = sys.argv[sys.argv.index('-a')+1]
+
+# print(sys.argv)
+# print(getUrl)
+# print(getAttackName)
+
+
+# for string in sys.argv:
+#     if((string == '-h' or string == '--help')and string in helpCommand):
+#         print(helpCommand)
+#         exit()
+
+
+match getAttackName:
+    case 'sqlinjection':
+        sqliscanner.sqlinjection(getUrl)
